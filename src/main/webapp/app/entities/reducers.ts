42e43bee558aa24ad7482e149a567e89b9cbcfbf
bigdatalille <bigdatalille@gmail.com>
2023-02-15 16:04:11 +0100

    first commit

--- src/main/webapp/app/entities/reducers.ts
+++ src/main/webapp/app/entities/reducers.ts
@@ -1,6 +1,14 @@
+import category from 'app/entities/category/category.reducer';
+import product from 'app/entities/product/product.reducer';
+import customer from 'app/entities/customer/customer.reducer';
+import address from 'app/entities/address/address.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
+  category,
+  product,
+  customer,
+  address,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -3,6 +3,10 @@ import { Route } from 'react-router-dom';
 
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 
+import Category from './category';
+import Product from './product';
+import Customer from './customer';
+import Address from './address';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default () => {
@@ -10,6 +14,10 @@ export default () => {
     <div>
       <ErrorBoundaryRoutes>
         {/* prettier-ignore */}
+        <Route path="category/*" element={<Category />} />
+        <Route path="product/*" element={<Product />} />
+        <Route path="customer/*" element={<Customer />} />
+        <Route path="address/*" element={<Address />} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </ErrorBoundaryRoutes>
     </div>
